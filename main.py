0694565e73951720f1ba99f04a8354ef96a30750
Vusal Ismayilov <woosal@protonmail.com>
2020-11-22 07:00:10 +0300

    1.0

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class Bot():
+    def __init__(self, message, event, eventTime):
+        self.message = message
+        self.event = event
+        self.eventTime = eventTime
+
+    def send_message(self, message, event, eventTime):
+        pass
+
+    def get_events(self, concurrentEvents=10):
+        pass
+
+##########################################################
+# SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON #
+# SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON #
+# SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON #
+# SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON SOON #
+##########################################################
